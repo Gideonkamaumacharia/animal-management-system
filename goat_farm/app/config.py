64f0acd9a35264e3bb1fde5486960bb6b155e1ac
Gideonkamaumacharia <kamaugedion95@gmail.com>
2025-10-01 09:22:44 +0300

    Modify config.py

--- goat_farm/app/config.py
+++ goat_farm/app/config.py
@@ -4,5 +4,5 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "dev_secret_key"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "goatfarm.db")
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://gideon:123password@localhost/goatfarm"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
