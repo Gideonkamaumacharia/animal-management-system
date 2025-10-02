2e81c01b5f7e3283e554d3f93d93ae48b8288871
Gideonkamaumacharia <kamaugedion95@gmail.com>
2025-10-02 14:44:54 +0300

    IMport and add treatments_bp to routes/__init__.py for registration in init.py

--- goat_farm/app/routes/__init__.py
+++ goat_farm/app/routes/__init__.py
@@ -1,5 +1,6 @@
 from .routes import main
 from .animal_routes import animals_bp
+from .treatment_routes import treatments_bp
 
 # Keep a list of all blueprints here
-all_blueprints = [main, animals_bp]
+all_blueprints = [main, animals_bp, treatments_bp]
