b85b4b9116335a7cca16ad0f07937c985ee68f59
Gideonkamaumacharia <kamaugedion95@gmail.com>
2025-10-05 22:25:45 +0300

    Update routes/__init__.py; add sales_bp for registration

--- goat_farm/app/routes/__init__.py
+++ goat_farm/app/routes/__init__.py
@@ -1,6 +1,7 @@
 from .routes import main
 from .animal_routes import animals_bp
 from .treatment_routes import treatments_bp
+from .sales_routes import sales_bp
 
 # Keep a list of all blueprints here
-all_blueprints = [main, animals_bp, treatments_bp]
+all_blueprints = [main, animals_bp, treatments_bp, sales_bp]
