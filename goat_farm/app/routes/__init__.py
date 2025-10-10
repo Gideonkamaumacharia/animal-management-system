865539560918e747b8eacab00204c0c1d6bb0e4a
Gideonkamaumacharia <kamaugedion95@gmail.com>
2025-10-10 22:41:08 +0300

    Add expense blueprint to the list of blueprints in routes/init.py for registration in the factory

--- goat_farm/app/routes/__init__.py
+++ goat_farm/app/routes/__init__.py
@@ -2,6 +2,7 @@ from .routes import main
 from .animal_routes import animals_bp
 from .treatment_routes import treatments_bp
 from .sales_routes import sales_bp
+from .expense_routes import expense_bp
 
 # Keep a list of all blueprints here
-all_blueprints = [main, animals_bp, treatments_bp, sales_bp]
+all_blueprints = [main, animals_bp, treatments_bp, sales_bp, expense_bp]
