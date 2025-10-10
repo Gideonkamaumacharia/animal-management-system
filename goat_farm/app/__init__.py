26d4a3302837045e39ff100166650b4e63dcd306
Gideonkamaumacharia <kamaugedion95@gmail.com>
2025-10-10 22:33:55 +0300

    Update factory to factor in testing;tranfer in memory db configuration to factory for testing

--- goat_farm/app/__init__.py
+++ goat_farm/app/__init__.py
@@ -1,24 +1,18 @@
 from flask import Flask
 from app.routes import all_blueprints
 from app.extensions import db ,migrate
+from app import models
 
 def create_app(config_class="app.config.Config"):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
-    # Initialize extensions
     db.init_app(app)
     migrate.init_app(app, db)
-
-    # Import models so they are registered with SQLAlchemy
-    from app import models
 
     # Register routes (blueprints)
     for bp in all_blueprints:
         app.register_blueprint(bp)  
-
-    with app.app_context():
-        db.create_all()
 
     return app
 
