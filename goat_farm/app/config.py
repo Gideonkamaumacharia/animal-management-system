6dc448abf82341c9a0c0cb351963acc01f3971fd
Gideonkamaumacharia <kamaugedion95@gmail.com>
2025-10-10 22:32:08 +0300

    Modify config.py; add test configurations to allow code testing

--- goat_farm/app/config.py
+++ goat_farm/app/config.py
@@ -6,3 +6,12 @@ class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "dev_secret_key"
     SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://gideon:123password@localhost/goatfarm"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class TestingConfig(Config):
+    """Configuration for testing."""
+    # This flag enables testing mode in Flask extensions
+    TESTING = True 
+    # This tells SQLAlchemy to use a temporary database in RAM
+    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
+    # You can also disable CSRF protection for tests if you use Flask-WTF
+    WTF_CSRF_ENABLED = False
